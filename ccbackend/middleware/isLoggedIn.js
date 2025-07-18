ee3607c1737012ffd822372292250fb374d16b63
Mukul-Goyal <mukulg756@gmail.com>
2025-07-18 14:58:04 +0530

    debugging

--- ccbackend/middleware/isLoggedIn.js
+++ ccbackend/middleware/isLoggedIn.js
@@ -2,9 +2,8 @@ import jwt from "jsonwebtoken";
 import UserModel from "../models/user-model.js";
 
 const isLoggedIn = async (req, res, next) => {
-  const token = req.cookies.token || req.headers['Authorization']?.split(" ")[1] ;
+  const token = req.cookies.token || req.headers['Authorization']?.split(" ")[1] || req.headers['authorization']?.split(" ")[1];
 console.log(token);
-
 
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
