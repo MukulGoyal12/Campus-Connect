2db67aac4b4d390363c3e8d0ef38520c92d84a2d
Mukul-Goyal <mukulg756@gmail.com>
2025-07-19 22:49:42 +0530

    Singnup bug fix 3

--- ccfrontend/src/Pages/Register.jsx
+++ ccfrontend/src/Pages/Register.jsx
@@ -31,7 +31,7 @@ const Register = () => {
 
       .then((res) => {
         console.log(res.data);
-        navigate("/login");
+        navigate("/auth/login");
       })
       .catch((err) => {
         console.error(err);
